998d3eb87fd6153b3808414fd22982894991f6a1
Simeon Nedkov <simeon@ndkv.nl>
2017-12-22 00:50:03 +0100

    finalise v0.1 of dapp

--- js/dapp.js
+++ js/dapp.js
@@ -1,33 +1,87 @@
 window.addEventListener('load', function() {
-  if (typeof web3 !== 'undefined') {
+  try {
     window.web3 = new Web3(web3.currentProvider);
 
-
-    CapsuleCoin = TruffleContract(CapsuleCoinABI);
+    const CapsuleCoin = TruffleContract(CapsuleCoinABI);
     CapsuleCoin.setProvider(web3.currentProvider);
 
+    // fetch the deployed instance of the contract
     CapsuleCoin.deployed().then(function(instance) {
-      startApp(instance);
+        startApp(instance, web3.eth.accounts[0]);
     });
-  } else {
-    console.log('Error: could not find web3. Please visit this page in MetaMask.');
+  }
+  catch (e) {
+      startApp(null, null);
 
-    document.getElementById('balance').innerText = 'undefined. Please visit this website in MetaMask.'
+      console.log('Error: could not find web3. Please visit this page in MetaMask.');
     }
 });
 
-function startApp(instance) {
-    var account = web3.eth.accounts[0];
-    var account = '0x5159d8C3BE83C21530F4AC43451e326B8FF5bbB0';
-    console.log(account);
+function startApp(instance, account) {
+    // var account = '0x5159d8C3BE83C21530F4AC43451e326B8FF5bbB0';
 
-    renderBalance(instance, account);
+    const vm = new Vue({
+        el: '#balance-component',
+        data: {
+           balance: 0,
+           message: craftMessage(instance, account),
+        },
+        computed: {
+            alertType: function () {
+                return {
+                    'show-display': this.message,
+                    'alert-danger': !instance,
+                    'alert-warning': !account,
+                    'alert-info': !this.balance
+                }
+            }
+        },
+        mounted: function() {
+            const self = this;
+
+            if (instance && account) {
+                getBalance(instance, account)
+                .then(function(result) {
+                    let balance = parseInt(result);
+
+                    if (balance === 0) {
+                        self.message = "Oh-oh, you don't have any coins. <a href=\"https://github.com/ndkv/capsule-coin#gimme-some-coins-please-\" class=\"alert-link\">Get some!</a>"
+                    } else {
+                        self.balance = balance;
+                    }
+                })
+            }
+        }
+    });
+
+    const explainer = new Vue({
+        el: '#explainer-component',
+        data: {
+            explainerShow: false,
+            description: 'What is this?'
+        },
+        methods: {
+            toggle: function () {
+                this.explainerShow = !this.explainerShow;
+
+                this.description = (this.explainerShow === true) ? 'Much nonsense! Wow!' : 'What is this?';
+            }
+        }
+    })
 }
 
-function renderBalance(instance, account) {
-    getBalance(instance, account).then(function(balance) {
-      document.getElementById('balance').innerText = balance;
-    });
+function craftMessage(instance, account) {
+    let message = '';
+
+    if (instance === null) {
+        message = 'Please <a href="https://github.com/ndkv/capsule-coin#gimme-some-coins-please-" class="alert-link">install Metamask</a>.';
+    }
+
+    if (account === undefined) {
+        message = 'Please unlock your Metamask account.';
+    }
+
+    return message;
 }
 
 function getBalance(instance, account) {
